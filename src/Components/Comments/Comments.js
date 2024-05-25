d8eef53d860fb27c62c1b37dd11289b6cbf432c5
unknown <sean.dev.davis@.com>
2024-05-24 19:02:22 -0500

    comments showing correctly in modal

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -3,15 +3,19 @@ import CommentsCard from "./CommentsCard";
 
 function Comments() {
   const comments = require("../../data/comments.json")
+
   return (
     <div className="comments container">
       <div className="row">
-        <div className="col-12">
+        <div className="col-4">
+          <p>Leave a comment:</p>
+          </div>
+        <div className="col-8">
           <input type="text" className="comment-input" placeholder="Talk about this hat..." />
-          {comments.map((comment) => (
-            <CommentsCard key={comment.id} comment={comment}/>
-          ))}
         </div>
+          {comments.map((comment) => (
+            <CommentsCard key={comment.id} comment={comment} />
+          ))}
       </div>
     </div>
   );
--- src/Components/Comments/CommentsCard.js
+++ src/Components/Comments/CommentsCard.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-function CommentsCard() {
+function CommentsCard(props) {
+  const hatInfo = require("../../data/hats.json");
+  hatInfo.map((hat) => {
+    return hat;
+  });
+
+  const comments = props.comment;
+  console.log(comments);
   return (
     <div className="comment-card">
-      <input type="text" />
+      <section className="comment-card-box">
+        <section className="commenter-info">user</section>
+        <hr className="comment-hr" />
+        <section className="comment-section">{comments.comment}</section>
+        <hr className="comment-hr" />
+        <section className="comment-date">{comments.date}</section>
+      </section>
     </div>
   );
 }
