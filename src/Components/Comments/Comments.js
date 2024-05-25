1c475a22ef501aeccae998bf310a47dd7dd3579a
unknown <sean.dev.davis@.com>
2024-05-24 20:37:39 -0500

    fixed comments in the modal more

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -2,21 +2,30 @@ import React from "react";
 import CommentsCard from "./CommentsCard";
 
 function Comments() {
-  const comments = require("../../data/comments.json")
+  const comments = require("../../data/comments.json");
 
   return (
     <div className="comments container">
-      <div className="row">
-        <div className="col-4">
-          <p>Leave a comment:</p>
+      <section className="comment-header">
+        <div className="row">
+          <div className="col-3 input-title">
+            <p>Leave a comment:</p>
           </div>
-        <div className="col-8">
-          <input type="text" className="comment-input" placeholder="Talk about this hat..." />
+          <div className="col-9 input-comment">
+            <input
+              type="text"
+              className="comment-input"
+              placeholder="Talk about this hat..."
+            />
+          </div>
         </div>
-          {comments.map((comment) => (
-            <CommentsCard key={comment.id} comment={comment} />
-          ))}
-      </div>
+      </section>
+      <hr span="separator"/>
+      <section className="comment-reader">
+        {comments.map((comment) => (
+          <CommentsCard key={comment.id} comment={comment} />
+        ))}
+      </section>
     </div>
   );
 }
--- src/Components/HatSection/HatSectionCard.js
+++ src/Components/HatSection/HatSectionCard.js
@@ -8,18 +8,10 @@ function HatSectionCard(props) {
     <div className="col hat-card text-center">
       <div className="text-center">
         <div className="card" ref={hats.id}>
-          {/* <div className="hat-card"> */}
           <div className="card-body">
-            {/* <Link to={singleLink}><img src={hats.imageUrl} className="card-img-top" alt="Pic of a hat" /></Link> */}
             <h3 className="card-title hat-name">{hats.name}</h3>
             <img className={hats.className} src={hats.img} alt={hats.alt} />
-            {/* <h5 className="card-text hat-type">{hats.type}</h5>
-            <h5 className="card-text hat-color">{hats.mainColor}</h5>
-            <div className="col-10 offset-1">
-              <p className="card-text hat-desc">{hats.description}</p>
-            </div> */}
           </div>
-          {/* <Comments /> */}
         </div>
       </div>
 
@@ -32,7 +24,6 @@ function HatSectionCard(props) {
         aria-labelledby="exampleModalToggleLabel"
         tabindex="-1"
       >
-        {/* First Modal */}
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
@@ -49,9 +40,7 @@ function HatSectionCard(props) {
             <div className="modal-body">
               <div className="text-center">
                 <div className="card" ref={hats.id}>
-                  {/* <div className="hat-card"> */}
                   <div className="card-body">
-                    {/* <Link to={singleLink}><img src={hats.imageUrl} className="card-img-top" alt="Pic of a hat" /></Link> */}
                     <img
                       className="modal-hat-pic"
                       src={hats.img}
@@ -79,43 +68,13 @@ function HatSectionCard(props) {
         aria-labelledby="exampleModalToggleLabel2"
         tabindex="-1"
       >
-        {/* Second Modal */}
-        {/* <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalToggleLabel2">
-                Modal 2
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              Hide this modal and show the first with the button below.
-            </div>
-            <div className="modal-footer">
-              <button
-                className="btn btn-primary"
-                data-bs-target="#exampleModalToggle"
-                data-bs-toggle="modal"
-              >
-                Back to first
-              </button>
-            </div>
-          </div>
-        </div> */}
       </div>
       <button
-        className="btn btn-primary hat-details-btn"
+        className="btn btn-dark hat-details-btn"
         data-bs-target="#exampleModalToggle"
         data-bs-toggle="modal"
-        // type="button"
-        // aria-label="Close"
       >
-        Hat Details
+        <p>Hat Details</p>
       </button>
     </div>
   );
