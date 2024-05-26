fe1832f0340e570ba45de48d2f268cf28902b7d8
Sean Davis <sean.dev.davis@gmail.com>
2024-05-26 18:51:33 -0500

    added navbar and fixed the username display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import Profile from './Components/Profile/Profile';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Profile from "./Components/Profile/Profile";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" Component={Profile} />
+          {/* <Route exact path="/departments" Component={Departments} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
