67fe67029296a499274df43ef7e99a6126fb2590
Sean Davis <sean.dev.davis@gmail.com>
2024-06-05 12:53:31 -0500

    added signin and signup form to page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from 'react-router-dom'
 
 export default function Login() {
+  const loginUser = (e) => {
+    e.preventDefault();
+
+    console.log("user logged in...");
+  };
   return (
-    <div>
-      <h1 className="login text-center">Login</h1>
+    <div className="container">
+      <h1 className="text-center">Login to Cap-stone</h1>
+      <div className="row">
+        <div className="login col-6 offset-3 text-center">
+          <form>
+            <label htmlFor="username">Username:</label>
+            <br />
+            <input type="text" name="username" placeholder=" Username..." />
+            <br />
+            <br />
+            <label htmlFor="password">Password:</label>
+            <br />
+            <input type="password" name="password" placeholder=" Password..." />
+            <br />
+            <br />
+            <Link to="/">
+              <button className="btn btn-light" onClick={loginUser}>
+                Login
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
