208e293f06b69eca3fc013efec5e35c986fa38cd
Sean Davis <sean.dev.davis@gmail.com>
2024-06-04 22:44:43 -0500

    got all hats showing for main user

--- src/Components/HatSection/UserHatsCard.js
+++ src/Components/HatSection/UserHatsCard.js
@@ -3,9 +3,10 @@ import React from "react";
 export default function UserHatsCard(props) {
   console.log(props);
   return (
-    <div>
-      <section className="hatCard-header"></section>
-      <section className="hatCard-header"></section>
+    <div className="col-6 text-center">
+      <h3>Hats</h3>
+      {/* <section className="hatCard-header">Hat</section>
+      <section className="hatCard-header"></section> */}
     </div>
   );
 }
